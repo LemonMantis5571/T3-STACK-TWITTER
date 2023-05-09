518138b02063feb774a9991da4a3cd6678f65267
LemonMantis5571 <briter456@gmail.com>
2023-05-09 16:27:21 -0600

    Tweet Button and SideNav

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+export default function NewTweetForm() {
+  return (
+    <form className='flex flex-col gap-2 border-2 px-4 py-2'>
+        <div className='flex gap-4'>
+            {/* <ProfileImage src="url" /> */}
+            <textarea className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none' placeholder="What's happening?" />
+        </div>
+       <Button className='self-end'>Tweet</Button>
+    </form>
+  )
+}
